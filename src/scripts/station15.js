2273ca4b841337d025a098bc239d1abacf8cc188
k-tsunoda615 <kntrtnd@gmail.com>
2025-02-09 15:33:15 +0900

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,41 @@
-async function getData() {
-  // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
-  // ３秒後にフルネームのリストを表示されるようにしましょう。
-  // 最初から表示されていると、クリアになりません。
-  const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+const { set } = require("fp-ts");
+
+function getData() {
+  // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
+  // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
+  // そのため、JavaScriptで連結させて生成することにしました。
+  const test = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  return Promise.all(test.map(buildFullName));
 }
 
-function test() {
-  return
+function buildFullName(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        ...data,
+        full_name: `${data.family_name} ${data.first_name}`,
+      });
+    }, 3000);
+  });
 }
 
+// 出力が想定される答え
+// const test = [
+//     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affiliation: 'TechTrain', is_student: false},
+//     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affiliation: 'HogeHoge大学', is_student: true}
+// ];
